66e59ba4f7dc7069f00252464b6aa94776b5005e
Vaor22 <96342252+Vaor22@users.noreply.github.com>
2026-05-13 23:06:46 +0300

    Refactor TPQueue class formatting and structure

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,57 +4,57 @@
 
 #include <stdexcept>
 
-template<typename T>
+template <typename T>
 class TPQueue {
-private:
-    struct Node {
-        T data;
-        Node* next;
-    };
-    Node* head;
+ private:
+  struct Node {
+    T data;
+    Node* next;
+  };
+  Node* head;
 
-public:
-    TPQueue() : head(nullptr) {}
+ public:
+  TPQueue() : head(nullptr) {}
 
-    ~TPQueue() {
-        while (head) {
-            Node* temp = head;
-            head = head->next;
-            delete temp;
-        }
+  ~TPQueue() {
+    while (head) {
+      Node* temp = head;
+      head = head->next;
+      delete temp;
     }
+  }
 
-    void push(const T& value) {
-        Node* newNode = new Node{value, nullptr};
+  void push(const T& value) {
+    Node* newNode = new Node{value, nullptr};
 
-        if (!head || head->data.prior < value.prior) {
-            newNode->next = head;
-            head = newNode;
-        } else {
-            Node* current = head;
-            while (current->next && current->next->data.prior >= value.prior) {
-                current = current->next;
-            }
-            newNode->next = current->next;
-            current->next = newNode;
-        }
+    if (!head || head->data.prior < value.prior) {
+      newNode->next = head;
+      head = newNode;
+    } else {
+      Node* current = head;
+      while (current->next && current->next->data.prior >= value.prior) {
+        current = current->next;
+      }
+      newNode->next = current->next;
+      current->next = newNode;
     }
+  }
 
-    T pop() {
-        if (!head) {
-            throw std::runtime_error("Queue is empty");
-        }
-        Node* temp = head;
-        T value = temp->data;
-        head = head->next;
-        delete temp;
-        return value;
+  T pop() {
+    if (!head) {
+      throw std::runtime_error("Queue is empty");
     }
+    Node* temp = head;
+    T value = temp->data;
+    head = head->next;
+    delete temp;
+    return value;
+  }
 };
 
 struct SYM {
-    char ch;
-    int prior;
+  char ch;
+  int prior;
 };
 
 #endif  // INCLUDE_TPQUEUE_H_
